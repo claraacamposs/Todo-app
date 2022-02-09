60db3378970ef5ae830152011b4b99a8b3fbc9ef
ruanduarte03 <ruanduarte0300@gmail.com>
2022-02-09 17:56:36 -0300

    Todo App

---
+++ src/pages/TodoApp/TodoApp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TodoCreator from './containers/TodoCreator/TodoCreator'
+import TodoList from './containers/TodoList/TodoList'
+import TodoFilter from './containers/TodoFilter/TodoFilter'
+
+
+
+function TodoApp() {
+    return (
+        <>
+            <TodoCreator/>
+            <TodoList/>
+            <TodoFilter/>
+        </>
+    )
+}
+
+export default TodoApp
